f6c3a024ea1da36a7c6b1a36ec05b06a43ea2ecf
Andrej Sýkora <as@andrejsykora.com>
2018-02-03 18:29:36 +0100

    fix - footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
   .container
     .content.has-text-centered
       p
-        | FamuPAD s ❤ Pepa, Andrej, Damián &copy; 2017
+        | FamuPAD s ❤ Pepa, Andrej, Damián &copy; 2018
       p
         | Administrace a dotazy: famupad@gmail.com
       p
